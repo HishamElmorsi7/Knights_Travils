db489ffbea16327f23908345f55e652b9f46a707
HishamElmorsi7 <74022699+HishamElmorsi7@users.noreply.github.com>
2021-09-05 07:29:49 +0200

    Printing chess board

--- knights_travils.rb
+++ knights_travils.rb
@@ -1,1 +1,27 @@
 require_relative "./PolyTreeNode/PolyTreeNode.rb"
+#KFS => Knight Path Finder
+class KFS
+    attr_reader :chess_board
+
+    def initialize
+        @chess_board = Array.new(8){ Array.new(8, "[ ]")}
+    end
+
+    def print_chess_board
+        print "  "
+        (0..7).each {|col_idx| print " #{col_idx} "}
+        puts
+
+        row_idx = 0
+        self.chess_board.each do |row|
+            print "#{row_idx} "
+            row_idx += 1
+            row.each { |position| print position }
+            puts
+        end
+        
+        return
+    end
+
+
+end
